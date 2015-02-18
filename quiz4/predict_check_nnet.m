7d23b05e9b2b497a44a5bd4aa0dd815cb0dd10f2
Lixinyi-DUT <lxy201292241@mail.dlut.edu.cn>
2015-02-19 00:53:39 +0800

    use 0-1 error instead of squared error

--- quiz4/predict_check_nnet.m
+++ quiz4/predict_check_nnet.m
@@ -2,8 +2,7 @@ function re=predict_check_nnet(w1,w2,test)
  re=0;
   N=length(test);
      for i=1:N
-        predict=tanh(w2'*[1;tanh(w1'*[1,test(i,1:2)]')]);
-        re=re+(test(i,3)-predict)^2;
+        predict(i,1)=sign(tanh(w2'*[1;tanh(w1'*[1,test(i,1:2)]')]));
      end
-     re=re/N;
+     re=sum(sign(abs(predict-test(:,3))))/N;
 end
